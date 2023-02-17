e30c4751be7493f74e325f1d58b479b4848c675a
Bruno Mota <bmotadev@gmail.com>
2023-02-17 00:54:05 -0300

    :sparkles: feat: classe cachorro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:exercicio_heranca/src/cachorro.dart';
 import 'package:exercicio_heranca/src/gato.dart';
 
 void main(List<String> args) {
   Gato gato = Gato(nome: "Miucha", barulho: "Miau");
   print("Gato faz ${gato.barulho}");
+
+  Cachorro cachorro = Cachorro(nome: "Pitoco", barulho: "Auau");
+  print("Cachorro faz ${cachorro.barulho}");
 }
--- lib/src/cachorro.dart
+++ lib/src/cachorro.dart
@@ -3,5 +3,6 @@ import 'package:exercicio_heranca/src/animal.dart';
 
 class Cachorro extends Animal {
   String nome;
-  
+
+  Cachorro({required super.barulho, required this.nome}); // construtor
 }
